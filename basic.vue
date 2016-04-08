c03b24e80e55106483220c02449da164c6bb25a7
Paul Pflugradt <paulpflugradt@googlemail.com>
2016-04-09 00:26:02 +0200

    Updates

---
+++ basic.vue
@@ -0,0 +1,34 @@
+<template lang="jade">
+.container
+  button click
+    modal
+      p Content
+  button not dismissable
+    modal(:dismissable="false")
+      p Content
+      closer
+  a(href="https://github.com/vue-comps/vue-comps-modal/blob/master/dev/basic.vue") source
+</template>
+
+<script lang="coffee">
+module.exports =
+  components:
+    "modal" : require "../src/modal.vue"
+    "closer":
+      template: "<button @click.prevent='close'>close</button>"
+      methods:
+        close: ->
+          @$dispatch "close"
+</script>
+
+<style lang="stylus">
+.container > a
+  position absolute
+  left 250px
+  top 40px
+.vc-modal
+  background white
+  width 80%
+  height 60%
+  margin auto
+</style>
